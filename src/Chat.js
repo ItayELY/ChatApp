a584ae48d5984b034565d8f77fa54b3f8117c902
Itay Elyashev <itay.elyashiv@gmail.com>
2022-04-03 15:37:14 +0300

    added messages segment

--- src/Chat.js
+++ src/Chat.js
@@ -31,8 +31,33 @@ function Chat() {
             {contacts.map((contact) =>{return <ContactItem contact={contact}></ContactItem>})}
           </ul>
           </div>
-      </div>
+          <div className="col h-100">
+  <div className="row list-group-item bg-light">
+    <div className="ms-2 me-auto">
+      <div className="fw-bold">Itay Elyashiv</div>
     </div>
+  </div>
+  <div className="row h-100  position-relative" style={{backgroundColor: 'lightskyblue'}}>
+    <div className="input-group mb-3 fixed-bottom position-absolute">
+      <div className="input-group-prepend">
+        <button type="button" className="btn btn-secondary ">send</button>
+      </div>
+      <input type="text" className="form-control" aria-label="Text input with segmented dropdown button" />
+      <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">attach</button>
+      <ul className="dropdown-menu">
+        <li><a className="dropdown-item" href="#">image</a></li>
+        <li><a className="dropdown-item" href="#">video</a></li>
+        <li><a className="dropdown-item" href="#">audio</a></li>
+        <li><a className="dropdown-item" href="#">location</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+      </div>
+      
+    </div>
+    
   </span>);
 }
 
