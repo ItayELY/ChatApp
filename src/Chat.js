bb4375738813dd6a27ec35c357c3813be94ce70b
Yona Dav <yonadav.le@gmail.com>
2022-04-10 11:46:53 +0300

    Seving on local storage image uploadied by users, still not displaying it on screen

--- src/Chat.js
+++ src/Chat.js
@@ -72,6 +72,28 @@ function Chat() {
 
   };
 
+  var getImageInput = () => {
+    console.log("upload Image");
+    // var fr=new FileReader();
+    // fr.onload=function(){
+    //     document.getElementById('output')
+    //             .textContent=fr.result;
+    // }
+
+    // fr.readAsText(this.files[0]);
+    var thisElement = document.getElementById("imageInput");
+    var reader = new FileReader();
+    reader.onload = function () {
+      var thisImage = reader.result;
+      localStorage.setItem("imgData", JSON.stringify(thisImage));
+    };
+    reader.readAsDataURL(thisElement.files[0]);
+    console.log('localStorage.getItem("imgData"): ', localStorage.getItem("imgData"));
+
+
+
+  };
+
 
 
   return (<span class="d-flex justify-content-center">
@@ -153,10 +175,10 @@ function Chat() {
                 <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z" />
               </svg>
             </button>
-            
+
             <ul className="dropdown-menu" aria-labelledby="dropdownAttach">
               <li><a className="dropdown-item" href="#">
-              <input type="file" style={{opacity: "0", zIndex: '-1'}} ></input>
+                <input id="imageInput" onChange={getImageInput} type="file" style={{ opacity: "0", zIndex: '-1' }} ></input>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
                   <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
                   <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z" />
