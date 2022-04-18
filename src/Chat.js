d3dec568dc5e8cd7c8175a5ccc8c0ed8123ffdb9
Yona Dav <yonadav.le@gmail.com>
2022-04-18 16:41:17 +0300

    arranged buttons on toggle bar to appear more orderly

--- src/Chat.js
+++ src/Chat.js
@@ -11,7 +11,8 @@ import ChatImageMessage from './ChatImageMessage'
 import TakeSelfie from "./TakeSelfie";
 import AudioRecorder from "./AudioRecorder";
 import ChatAudioMessage from "./ChatAudioMessage";
-import ChatVideoMessage from "./ChatVideoMessage"; 
+import ChatVideoMessage from "./ChatVideoMessage";
+import React from "react"; 
 
 
 // if (!localStorage.getItem("storedUsersList")){
@@ -22,6 +23,7 @@ import ChatVideoMessage from "./ChatVideoMessage";
 // }
 
 //localStorage.removeItem("storedUsersList")
+console.log("chats render");
 
 
 var derivedUsersList;
@@ -183,6 +185,16 @@ function Chat() {
 
   };
   
+  var scrollDownMessagesBar = () => {
+    console.log("scrollscrollscrollscrollscrollscrollscrollscrollscroll  scroll");
+    document.getElementById("messagesBar").scrollTop = document.getElementById("messagesBar").scrollHeight;
+    
+  };
+
+  React.useEffect(() => {
+    // Runs after the first render() lifecycle
+    scrollDownMessagesBar();
+}, []);
 
 
   return (<span class="d-flex justify-content-center">
@@ -242,7 +254,8 @@ function Chat() {
                     var contactX = currentUserObject.userContacts.find(x => x.name === currentContact);
                     contactX.numOfUnreadMessages = "0";
                   }}><ContactItem contact={contact}></ContactItem></div>
-              })}
+              })
+              }
           </ul>
         </div>
         <div className="col h-100">
@@ -251,7 +264,7 @@ function Chat() {
               <div className="fw-bold">{currentContact}</div>
             </div>
           </div>
-          <div className="row h-100   list-group-flush col overflow-auto" style={{ backgroundColor: 'azure', }}>
+          <div id = "messagesBar" className="row h-100   list-group-flush col overflow-auto" style={{ backgroundColor: 'azure', }}>
 
 
 
@@ -281,7 +294,7 @@ function Chat() {
                     message.receiver === currentContact))
                   return <ChatVideoMessage className="align-self-end " message={message} sentByCurrentUser={message.writer === currentUserName ? true : false} align={"align-content-end"}></ChatVideoMessage>
               }
-
+              
 
 
             })}
@@ -307,7 +320,7 @@ function Chat() {
 
               
               <li><a className="dropdown-item" href="#">
-                <input id="imageInput" onChange={getImageInput} type="file" accept="image/*" style={{ opacity: "0", width: "20%" }}   ></input>
+                <input id="imageInput" onChange={getImageInput} type="file" accept="image/*" style={{ opacity: "0", width: "20%" }} hidden   ></input>
                 <div id="imageInputIcon" onClick={clickImageInputIcon}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
                     <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
@@ -317,7 +330,7 @@ function Chat() {
               </a></li>
 
               <li><a className="dropdown-item" href="#">
-                <input id="videoInput" onChange={getVideoInput} type="file" accept="video/*" style={{ opacity: "0", width: "20%" }}   ></input>
+                <input id="videoInput" onChange={getVideoInput} type="file" accept="video/*" style={{ opacity: "0", width: "20%" }} hidden  ></input>
                 <div id="videoInputIcon" onClick={clickVideoInputIcon}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
                     <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0v6h8V1H4zm8 8H4v6h8V9zM1 1v2h2V1H1zm2 3H1v2h2V4zM1 7v2h2V7H1zm2 3H1v2h2v-2zm-2 3v2h2v-2H1zM15 1h-2v2h2V1zm-2 3v2h2V4h-2zm2 3h-2v2h2V7zm-2 3v2h2v-2h-2zm2 3h-2v2h2v-2z" />
@@ -376,7 +389,6 @@ function Chat() {
 
 
     </div>
-
 
   </span>);
 }
